7d4e536e55ec03a1d4df88b568dbbaefcd4541c1
davijsf <davif2717@gmail.com>
2026-03-09 16:48:41 -0300

    feat: Adição de Service 'ClienteService', e suas respectivas funções.

---
+++ src/EcoSys.Core/Services/ClienteService.cs
@@ -0,0 +1,52 @@
+namespace EcoSys.Core.Entities;
+
+public class ClienteService
+{
+public Cliente CadastrarCliente(
+    Empresa empresa,
+    string nome, 
+    string email, 
+    string senha,
+    string login)
+    {
+
+    bool loginExiste = empresa.Clientes.Any(c => c.Login == login);
+
+    if(loginExiste)
+    {
+        throw new Exception("Login já existente.");
+    }
+
+    Cliente cliente = new Cliente
+    {
+        Nome = nome,
+        Email = email,
+        Login = login,
+        Senha = senha
+    };
+
+    // Adiciona à lista de clientes da classe Empresa
+    empresa.Clientes.Add(cliente);
+    return cliente;
+}
+
+    // Busca de cliente por login
+    // Uso do ? | pode retornar um vazio
+    public Cliente? BuscarClientePorLogin(List<Cliente> clientes, string login)
+    {
+        // retorna o primeiro cliente da lista | onde o login é igual ao valor do parâmetro
+        return clientes.FirstOrDefault(c => c.Login == login);
+    }
+
+    // Autenticação de cliente
+    public Cliente? Autenticar(List<Cliente> clientes, string login, string senha)
+    {
+        return clientes.FirstOrDefault(c => c.Login == login && c.Senha == senha);
+    }
+
+    // Listar compras do cliente
+    public List<Compras> ListarCompras(Cliente cliente)
+    {
+        return cliente.HistoricoCompras;
+    }
+}
