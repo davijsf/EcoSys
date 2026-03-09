5246e4f2125dbb12ccd73b94400318fc7e432099
davijsf <davif2717@gmail.com>
2026-03-09 16:41:53 -0300

    feat: Adição de atributos à entitie 'Cliente'.

--- src/EcoSys.Core/Entities/Cliente.cs
+++ src/EcoSys.Core/Entities/Cliente.cs
@@ -3,7 +3,12 @@ namespace EcoSys.Core.Entities;
 public class Cliente
 {
     public string Nome {get ; set ;}
-    public string Email {get ; set ;}
 
+    // Email e senha podem ser nulos, caso seja Loja física.
+    public string? Email {get ; set ;}
+    public string? Senha {get ; set ;}
+    public string? Login {get ; set ;}
+    
+    // Lista de compras do Cliente
     public List<Compras> HistoricoCompras {get ; set ;} = new List<Compras>();
 }
