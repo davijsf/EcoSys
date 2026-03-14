2a0ad7c10661fc3603c222606114fa90f4f96721
davijsf <davif2717@gmail.com>
2026-03-14 13:54:18 -0300

    feat: ajuste de lógica em método CadstrarProduto.

--- src/EcoSys.Core/Services/CompraService.cs
+++ src/EcoSys.Core/Services/CompraService.cs
@@ -5,20 +5,40 @@ using EcoSys.Core.Entities;
 
 public class CompraService
 {
-    public Compra RegistrarCompra( Cliente ? cliente, Loja ? loja, List<ItemCompra> itens, CanalVenda canalVenda)
+    public Empresa? empresa;
+
+    public CompraService (Empresa? empresa = null)
     {
+        this.empresa = empresa;
+    } 
+
+    public Compra RegistrarCompra( Cliente ? cliente, Loja loja, List<ItemCompra> itens, CanalVenda canalVenda)
+    {
+
+        // Valida itens
+        if (itens == null || !itens.Any())
+        {
+            throw new ArgumentException("Itens não podem estar vazios.");
+        }
+
+
         Compra compra = new Compra
         {
             Cliente = cliente,
-            Loja = loja,
+            Loja =  loja,
             Itens = itens,
             CanalVenda = canalVenda,
-            DataCompra = DateTime.Now
+            DataCompra = DateTime.Now,
+            Total = itens.Sum(i => i.SubTotal)
         };
 
-        // Verifica se o cliente não é null | caso seja loja física
+        // Salva na empresa (principal)
+        empresa?.Compras.Add(compra);
+
+        // Adiciona no histórico do cliente (se existir)
         if (cliente != null)
-        {
+        {   
+            cliente.HistoricoCompras ??=new List<Compra>();
             cliente.HistoricoCompras.Add(compra);
         }
         return compra;
@@ -26,7 +46,10 @@ public class CompraService
 
     public List<Compra> ListarCompras(Cliente cliente)
     {
-        return cliente.HistoricoCompras;
+        return empresa?.Compras?
+        .Where(c => c?.Cliente != null && c.Cliente.Login == cliente.Login)
+        .OrderByDescending(c => c.DataCompra)
+        .ToList() ?? new List<Compra>();
     }
 
     public double CalcularTotal(Compra compra)
