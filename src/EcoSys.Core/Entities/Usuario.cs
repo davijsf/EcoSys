5c726ec3cc0711928388dac93a9a50a754f67dd6
davijsf <davif2717@gmail.com>
2026-03-16 13:09:23 -0300

    feat: remocao de atributo Cargo em usuario. Devido non-necessidade

--- src/EcoSys.Core/Entities/Usuario.cs
+++ src/EcoSys.Core/Entities/Usuario.cs
@@ -8,6 +8,4 @@ public class Usuario
     public string Senha { get; set; } = "";
 
     public TipoUsuario Tipo { get; set; } // funcionário ou cliente
-    
-    public Cargo ? Cargo {get ; set ;} // (nullabe) para caso seja cliente
 }
