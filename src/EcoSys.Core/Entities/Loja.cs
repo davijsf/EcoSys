ac7f1b4e5c22a40c7726add8f13ea5b1bdd4834a
davijsf <davif2717@gmail.com>
2026-03-16 13:07:42 -0300

    fix: ajust of  non-nullable em all atributes in Loja.

--- src/EcoSys.Core/Entities/Loja.cs
+++ src/EcoSys.Core/Entities/Loja.cs
@@ -2,10 +2,8 @@ namespace EcoSys.Core.Entities;
 
 public class Loja : Empresa
 {
-    public string Cidade{get ; set ;}
-    public string Endereco {get ; set ;}
-    public string HorarioAbertura {get ; set ;}
-    public string HorarioFechamento {get ; set ;}
-
-    public List<Funcionario> Funcionarios {get ; set ;} = new List<Funcionario>(); 
+    public string ?Cidade{get ; set ;}
+    public string ?Endereco {get ; set ;}
+    public string ?HorarioAbertura {get ; set ;}
+    public string ?HorarioFechamento {get ; set ;}
 }
