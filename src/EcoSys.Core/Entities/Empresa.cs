13ac681cb9529255b9b8e6bc69a5c1e12959824e
davijsf <davif2717@gmail.com>
2026-03-27 15:45:50 -0300

    feat: Mudança de implementação para List<Produto>. Adição de lista segura.

--- src/EcoSys.Core/Entities/Empresa.cs
+++ src/EcoSys.Core/Entities/Empresa.cs
@@ -4,26 +4,18 @@ public class Empresa {
     public string ? Nome {get ; set ;}
     
     public List<Loja> Lojas {get ; set ;} = new List<Loja>();
-
-    // Lista de clientes
     public List<Cliente> Clientes {get ; set;} = new List<Cliente>();  
-
-    // Lista de funcionários
     public List<Funcionario> Funcionarios {get; set;} = new List<Funcionario>(); 
 
-    // Lista de produtos
-    public List<Produto> Produtos {get; set ;} = new List<Produto>();
+    private readonly List<Produto> _produtos = new();
+    public IReadOnlyList<Produto> Produtos => _produtos;
 
-    // Lista de categorias
+    internal void AdicionarProduto(Produto p) => _produtos.Add(p);
+    internal void RemoverProduto(Produto p) => _produtos.Remove(p);
+
     public List<Categoria> Categorias {get; set;} = new List<Categoria>();
-
-    // Lista de tags
     public List<Tag> Tags {get; set;} = new List<Tag>();
-
-    // Lista de compras dos clientes -- lista de vendas da empresa 
     public List<Compra> Compras {get ; set ;} = new List<Compra>();
-
-    // Lista de usuários 
     public List<Usuario> Usuarios {get ; set ;} = new List<Usuario>();
     
 }
